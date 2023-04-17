7926480086cf8127a01eca51d000e77e82c89c79
kj7779 <kojistill1212@outlook.jp>
2023-04-18 00:01:47 +0900

    fix

--- src/Controller/Diary3Controller.php
+++ src/Controller/Diary3Controller.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/diary3')]
 class Diary3Controller extends AbstractController
 {
     #[Route('/', name: 'app_diary3_index', methods: ['GET'])]
--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
-    #[Route('/')]
+    #[Route('lucky/number')]
 public function number(): Response
 {
 $number = random_int(0, 100);
